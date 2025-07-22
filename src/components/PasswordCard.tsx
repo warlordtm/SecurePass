d933faaeddd98c321df00bb8d608849a097dba94
Godwin Bamisaye <godwinbamisaye3@gmail.com>
2025-07-22 12:38:33 +0100

    Fix UI design

--- src/components/PasswordCard.tsx
+++ src/components/PasswordCard.tsx
@@ -1,4 +1,5 @@
 import "../styles/PasswordCard.css"
+import { useState } from "react"
 
 type Props = {
   id: number
@@ -20,9 +21,39 @@ function PasswordCard({
   note,
   onDelete,
   onEdit
-}: Props) {
+}: Props) 
+{
+
+  const [seen, setSeen] = useState(false)
   return (
-    <div className="password-card-section">
+    <section className="password-card-section">
+      <div className="user-details">
+        <p className="website">{website || "No Website"}</p>
+        <p className="username-or-email">{email || "N/A"}</p>
+        <p className="username">{username || "N/A"}</p>
+        {!seen ? ".........." : <p>{password}</p>}
+        <p>Note: {note || "None"}</p>
+      </div>
+      <div className="user-icons">
+        <div className="icons" onClick={() => setSeen(prev => !prev)}>
+          <img src={ seen ? "/src/assets/eye.svg" : "/src/assets/unmask.svg"} alt="eye-icon" />
+        </div>
+        <div className="icons">
+          <img src="/src/assets/copy.svg" alt="copy-icon" />
+        </div>
+        <div className="icons" onClick={() => onDelete(id)}>
+          <img src="src/assets/delete.svg" alt="icon" />
+        </div>
+        <div onClick={() => onEdit(id)} className="edit-btn icons">edit</div>
+      </div>
+    </section>
+  )
+}
+
+export default PasswordCard
+
+
+/* <div className="password-card-section">
       <div className="user-details">
         <h3>{website || "No Website"}</h3>
         <p>Email: {email || "N/A"}</p>
@@ -34,29 +65,4 @@ function PasswordCard({
         <button onClick={() => onEdit(id)} className="edit-btn icons">Edit</button>
         <button onClick={() => onDelete(id)} className="delete-btn icons">Delete</button>
       </div>
-    </div>
-
-
-<section className="password-card-section">
-      <div className="user-details">
-        <p className="website">{website || "No Website"}</p>
-        <p className="username-or-email">{email || "N/A"}</p>
-        <p className="username">{username || "N/A"}</p>
-        <p>{password}</p>
-      </div>
-      <div className="user-icons">
-        <div className="icons">
-          <img src="/src/assets/notifications.svg" alt="icon" />
-        </div>
-        <div className="icons">
-          <img src="/src/assets/search.svg" alt="icon" />
-        </div>
-        <div className="icons">
-          <img src="src/assets/voice-search-icon.svg" alt="icon" />
-        </div>
-      </div>
-    </section>
-  )
-}
-
-export default PasswordCard
+    </div> */
