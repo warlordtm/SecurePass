1600e057e5d81b98edabcce5e49ca8218e05b0b8
Godwin Bamisaye <godwinbamisaye3@gmail.com>
2025-07-23 09:49:08 +0100

    Configure to use vercel

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <HashRouter basename="/securepass">
+   <HashRouter>
     <App />
    </HashRouter>
   </StrictMode>,
